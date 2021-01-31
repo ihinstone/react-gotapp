31f233a6efff81fdf45ba8f32e64c8684f03f26f
Kamenev Nikolay <79969997909@mail.ru>
2021-01-31 12:14:00 +0300

    Create spinner.js

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './spinner.css';
+
+
+export const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-8cchz74skjd"><div className="ldio-nj6v1qqhlta">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
