deada49a0a14595517b6a44c4aeaaa60d7366df2
Kamenev Nikolay <79969997909@mail.ru>
2021-01-31 11:29:48 +0300

    add random character

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,29 +1,51 @@
 import React, {Component} from 'react';
 import './randomChar.css';
+import gotService from '../../services/gotService';
 
 export default class RandomChar extends Component {
 
+    constructor() {
+        super();
+        this.updateChar();
+    }
+
+    gotService = new gotService();
+    state = {
+        char: {}
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({char})
+    }
+
+    updateChar() {
+        const id = Math.floor(Math.random()*140 + 25);
+        this.gotService.getCharacter(id)
+            .then(this.onCharLoaded);
+    }
+
     render() {
+        const {char: { name, gender, born, died, culture}} = this.state;
 
         return (
             <div className="random-block rounded">
-                <h4>Random Character: John</h4>
+                <h4>Random Character: {name}</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Gender </span>
-                        <span>male</span>
+                        <span>{gender}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Born </span>
-                        <span>11.03.1039</span>
+                        <span>{born}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Died </span>
-                        <span>13.09.1089</span>
+                        <span>{died}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Culture </span>
-                        <span>Anarchy</span>
+                        <span>{culture}</span>
                     </li>
                 </ul>
             </div>
--- src/services/gotService.js
+++ src/services/gotService.js
@@ -21,12 +21,14 @@ export default class GotService {
         return this.getResource(`/books/${id}/`);
     }
     
-    getAllCharacters() {
-        return this.getResource(`/characters?page=5&pageSize=10`);
+    async getAllCharacters() {
+        const res = await this.getResource(`/characters?page=5&pageSize=10`);
+        return res.map(this._transformCharacter);
     }
     
-    getCharacter (id) {
-        return this.getResource(`/characters/${id}`);
+    async getCharacter (id) {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
     }
     
     getAllHouses() {
@@ -35,5 +37,35 @@ export default class GotService {
     
     getHouse(id) {
         return this.getResource(`/houses/${id}/`);
+    }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+    }
+
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapond: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publiser: book.publiser,
+            releader: book.releader
+        }
     }
 }
